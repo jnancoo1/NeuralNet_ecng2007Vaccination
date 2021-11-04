fc12171f0e117ac836939912929db6605a9ced7c
stephen200111 <92822991+stephen200111@users.noreply.github.com>
2021-11-04 19:36:35 -0400

    Update GUIandDatabase.py

--- GUIandDatabase.py
+++ GUIandDatabase.py
@@ -396,7 +396,7 @@ class MyLayout(ScreenManager):
 
         if temp == False:
             pass
-        elif int(year) > 2009 or int(year) < 1921:
+        elif int(year) > 2009 or int(year) < 1921 or len(year) > 4:
             toast("Invalid Date of Birth, Age out of Range")
             self.ids.year.text = ""
             temp = False
